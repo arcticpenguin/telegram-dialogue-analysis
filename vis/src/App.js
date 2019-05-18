18fab7797d50633bf37fc5eb9b645fdcbb6a039e
arcticpenguin <fmamimi@gmail.com>
2019-05-18 17:43:40 -0400

    update vis app

--- vis/src/App.js
+++ vis/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { uniqId } from 'lodash';
+import { uniqueId } from 'lodash';
+import { nest } from 'd3';
 
-import * as actions from "./actions";
+import * as actions from './actions';
+import TimelineVis from './components/TimelineVis';
 
 class App extends Component {
 
@@ -14,16 +16,26 @@ class App extends Component {
   render() {
     const { dialogueData } = this.props;
     console.log(dialogueData);
-    if(dialogueData.length === undefined) {
+    if (dialogueData.length === undefined) {
       return (
-        <div/>
+        <div />
       );
     }
 
+    // Process data
+    const timelineData = nest()
+      .key(d => d.dateStr)
+      .entries(dialogueData);
+    console.log(timelineData)
     return (
       <div>
+        <TimelineVis
+          data={[5, 4, 1, 3]}
+          width={800}
+          height={300}
+        ></TimelineVis>
         {dialogueData.map(dial => {
-          return <div>{dial.text}</div>
+          return <div key={uniqueId('dialogue-text-')}>{dial.text}</div>
         })}
       </div >
     );
