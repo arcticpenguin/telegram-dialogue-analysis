8487a2b11069bedb373fb1eed515209395f0cda0
arcticpenguin <fmamimi@gmail.com>
2019-05-18 00:14:49 -0400

    Updates

---
+++ precache-manifest.077476aae75cc71e5b7b610f24f299bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a23e0958272e6e4fee7e6de3c4118e",
+    "url": "/telegram-dialogue-analysis/index.html"
+  },
+  {
+    "revision": "944554c2f52af4012391",
+    "url": "/telegram-dialogue-analysis/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/telegram-dialogue-analysis/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "944554c2f52af4012391",
+    "url": "/telegram-dialogue-analysis/static/js/main.6162aaf4.chunk.js"
+  },
+  {
+    "revision": "477f9f2e2628c97955a0",
+    "url": "/telegram-dialogue-analysis/static/js/runtime~main.2433b7dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/telegram-dialogue-analysis/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/telegram-dialogue-analysis/vis/precache-manifest.8437453958ea87ffc5004d648b24b348.js"
+  "/telegram-dialogue-analysis/precache-manifest.077476aae75cc71e5b7b610f24f299bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/telegram-dialogue-analysis/vis/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/telegram-dialogue-analysis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
